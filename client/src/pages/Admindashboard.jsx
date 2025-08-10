6001d541d1b59de10fedafad0a80e6fc81c584fb
Mayank Agrawal <MayankAgrawal2005>
2025-08-10 14:09:30 +0530

    remove unnessary code

--- client/src/pages/Admindashboard.jsx
+++ client/src/pages/Admindashboard.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { AdminHeader } from '../components/AdminHeader';
-// import { FaCalendarDays } from "react-icons/fa6";
-// export const Admindashboard = () => {
-//   const { currentUser } = useSelector((state) => state.user);
-
-//   function getGreeting() {
-//     const currentHour = new Date().getHours();
-//     if (currentHour < 12) return 'Good Morning';
-//     if (currentHour < 18) return 'Good Afternoon';
-//     return 'Good Evening';
-//   }
-
-//   return (
-//     <div className='flex h-screen space-x-8'>
-//       {/* Left side: AdminHeader */}
-//       <div className='lg:w-[30%] w-full bg-slate-200 shadow-xl'>
-//         <AdminHeader />
-//       </div>
-
-//       {/* Right side: Main Content */}
-//       <div className=' lg:w-[70%] w-full flex-1 min-h-screen border border-gray-200 shadow-2xl  bg-white p-6 text-black'>
-//         <div className=' flex flex-col justify-center items-center min-h-screen  space-y-6'>
-//           <h1 className='text-3xl sm:text-5xl text-center flex-wrap'>Welcome To AttendMaster</h1>
-//           <p className='text-xl sm:text-2xl text-center font-bold'>
-//             {getGreeting()} {currentUser?.username}
-//           </p>
-//           <p className='mt-2 mb-10 text-xl hidden sm:inline font-mullish'>
-//             Select an option from the sidebar to get started.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 
 
